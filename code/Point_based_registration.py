d968b7d9ab142c0ba739719089af3581cb9b4130
Rick van Bergen <r.j.p.v.bergen@student.tue.nl>
2019-09-24 16:58:58 +0200

    beveiliging voor selecteren een punt

--- code/Point_based_registration.py
+++ code/Point_based_registration.py
@@ -7,6 +7,9 @@ import registration_util as util
 from IPython.display import display, clear_output
 
 def pbr(X,Xm):
+    if len(X)==2:
+        print('x must be larger than 1')
+        return
     X=util.c2h(X)
     Xm=util.c2h(Xm)
     T=reg.ls_affine(X,Xm)
