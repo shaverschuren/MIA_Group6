42de6f67dc0f55f7658dc919aa8b3f0ef4592c4b
Rick van Bergen <r.j.p.v.bergen@student.tue.nl>
2019-09-24 16:32:42 +0200

    Commit code vanuit charm

--- Point-based registration.py
+++ Point-based registration.py
@@ -1,1 +1,13 @@
-I_path='../data/image_data/'
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import registration as reg
+import registration_util as util
+from IPython.display import display, clear_output
+
+def pbr(X,Xm);
+    X=util.c2h(X)
+    Xm=util.c2h(Xm)
+    T=ls_affine(X,Xm)
+
